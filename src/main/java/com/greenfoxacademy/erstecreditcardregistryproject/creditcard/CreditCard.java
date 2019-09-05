e23231b1a147b7eebed971b48f9277f60b90e849
foldestibor <foldestiborlajos@gmail.com>
2019-09-05 12:08:01 +0200

    feat(hack) add contact details

--- src/main/java/com/greenfoxacademy/erstecreditcardregistryproject/creditcard/CreditCard.java
+++ src/main/java/com/greenfoxacademy/erstecreditcardregistryproject/creditcard/CreditCard.java
@@ -1,4 +1,25 @@
 package com.greenfoxacademy.erstecreditcardregistryproject.creditcard;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class CreditCard {
+
+  @Id
+  String creditCardNumber;
+
+  public CreditCard(){}
+
+  public CreditCard(String creditCardNumber){
+    this.creditCardNumber = creditCardNumber;
+  }
+
+  public String getCreditCardNumber() {
+    return creditCardNumber;
+  }
+
+  public void setCreditCardNumber(String creditCardNumber) {
+    this.creditCardNumber = creditCardNumber;
+  }
 }
