bc56a49eeb5efa0cc1649ea69c612e8a48cb54de
Zsolt M <hulfyback@gmail.com>
2019-09-05 11:27:17 +0200

    feat(erste): create new GET endpoint

---
+++ src/main/java/com/greenfoxacademy/erstecreditcardregistryproject/app/CreditCardRestController.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.erstecreditcardregistryproject.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ecards")
+public class CreditCardRestController {
+
+}
