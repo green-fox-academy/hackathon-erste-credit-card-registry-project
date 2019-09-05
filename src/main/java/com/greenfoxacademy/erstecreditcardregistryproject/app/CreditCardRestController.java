41aa0b624215712ff21e07f0e2325dcbe579de73
Zsolt M <hulfyback@gmail.com>
2019-09-05 20:20:36 +0200

    feat(erste): log INFO into file

--- src/main/java/com/greenfoxacademy/erstecreditcardregistryproject/app/CreditCardRestController.java
+++ src/main/java/com/greenfoxacademy/erstecreditcardregistryproject/app/CreditCardRestController.java
@@ -1,10 +1,9 @@
 package com.greenfoxacademy.erstecreditcardregistryproject.app;
 
-import com.greenfoxacademy.erstecreditcardregistryproject.creditcard.CreditCardDTO;
 import com.greenfoxacademy.erstecreditcardregistryproject.creditcard.CreditCardService;
-import com.greenfoxacademy.erstecreditcardregistryproject.utility.CreditCardUtil;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CreditCardRestController {
 
     @Autowired
-    CreditCardService creditCardService;
+    private CreditCardService creditCardService;
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(CreditCardRestController.class);
 
     @GetMapping("/{cardNumber}")
     public ResponseEntity<String> getCreditCardById(@PathVariable(name="cardNumber") String cardNumber) {
+        logger.info("Hello na!");
         return creditCardService.findByCardNumber(cardNumber);
     }
 }
