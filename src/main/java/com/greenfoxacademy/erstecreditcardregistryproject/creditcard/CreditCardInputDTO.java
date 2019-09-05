24626da9a03f190601c3783a39d68d5b94f27255
foldestibor <foldestiborlajos@gmail.com>
2019-09-05 21:28:50 +0200

    feat(hack)add register endpoint

---
+++ src/main/java/com/greenfoxacademy/erstecreditcardregistryproject/creditcard/CreditCardInputDTO.java
@@ -0,0 +1,20 @@
+package com.greenfoxacademy.erstecreditcardregistryproject.creditcard;
+
+import com.greenfoxacademy.erstecreditcardregistryproject.contactdetails.ContactDetailsDTO;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreditCardInputDTO {
+  private String type;
+  private String cardNumber;
+  private Integer cvv;
+  private String validThru;
+  private String owner;
+  private List<ContactDetailsDTO> contact;
+}
